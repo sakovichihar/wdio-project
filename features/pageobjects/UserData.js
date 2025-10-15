da495efcb8c0ff0db369e053aa040b78e1065d96
Ihar Sakovich <i.sakovich@jetmail.cc>
2025-10-15 10:51:09 +0300

    small fix from Zhenya

--- features/pageobjects/UserData.js
+++ features/pageobjects/UserData.js
@@ -3,7 +3,7 @@ require('dotenv').config();
     const values = {
 
         'Почта пользователя': process.env.EMAIL_ROX,
-        'Пароль пользоваетля': process.env.PASSWORD_ROX
+        'Пароль пользователя': process.env.PASSWORD_ROX
     }
 
 module.exports = { values };
