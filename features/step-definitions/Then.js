8d45e98498982a5d9cfca87e5ed3db3896d6f577
ihar <igorsakovich1995@gmail.com>
2025-10-24 20:17:26 +0300

    add final fix to steps

--- features/step-definitions/Then.js
+++ features/step-definitions/Then.js
@@ -1,16 +1,16 @@
 import { Then } from '@wdio/cucumber-framework';
 const { getLocator } = require('../element-mapped.js');
 
-Then('Я вижу {string}', async function (element) {
+Then('Я вижу {string} на странице {string}', async function (element, page) {
 
-    const locator = getLocator(element)
+    const locator = getLocator(element, page)
     await expect(locator).toBeDisplayed();
 }); 
 
-Then('Я вижу {string} и {string}', async function(element_1, element_2){
+// Then('Я вижу {string} и {string} на странице {string}', async function(element_1, element_2){
 
-    const locator_1 = getLocator(element_1)
-    const locator_2 = getLocator(element_2)
-    await expect(locator_1).toBeDisplayed();
-    await expect(locator_2).toBeDisplayed();
-});
+//     const locator_1 = getLocator(element_1)
+//     const locator_2 = getLocator(element_2)
+//     await expect(locator_1).toBeDisplayed();
+//     await expect(locator_2).toBeDisplayed();
+// });
--- features/step-definitions/When.js
+++ features/step-definitions/When.js
@@ -10,29 +10,29 @@ When('Я навожу на кнопку {string} в {string}', async function(el
     await button.moveTo();
 })
 
-// When('Я нажимаю кнопку {string} в {string}', async function(section, pageName) {
+When('Я нажимаю кнопку {string} в {string}', async function(element, page) {
 
-//     const button = place.button[section]
-//     await button.click()
-// });
+    const button = getLocator(element, page)
+    await button.click()
+});
 
-// When('Я ввожу {string} в {string}', async function(value, field){
-     
-//     const data = UserData.values[value]
-//     const input = getLocator(field)
-//     await input.setValue(data)
-// })
-
-// // если тест падает, а локатор внизу страницы, то можно поскроллить для отладки
-// When('Я скроллю страницу {string} до {string}', async function(location, element) {
-//     //добавить location
-//     const locator = getLocator(element)
-//     await locator.waitForDisplayed({ timeout: 5000 }); // сначала жду чтобы ебанный локатор появился в дом дереве
-//     await locator.scrollIntoView(); //потом скроллю ебучую страницу
-// });
-
-// //на всякий случай
-// When ('Я ожидаю заданное время', async function () {
+When('Я ввожу {string} в {string} на странице {string}', async function(value, field, page){
     
-//     await browser.pause(1000)
-// })
+    const data = UserData.values[value]
+    const input = getLocator(field, page)
+    await input.setValue(data)
+})
+
+// если тест падает, а локатор внизу страницы, то можно поскроллить для отладки
+When('Я скроллю страницу {string} до {string}', async function(page, element) {
+
+    const locator = getLocator(element, page)
+    await locator.waitForDisplayed({ timeout: 3000 }); // сначала жду чтобы ебанный локатор появился в дом дереве
+    await locator.scrollIntoView(); //потом скроллю ебучую страницу
+});
+
+//на всякий случай
+When ('Я ожидаю заданное время {string} секунды', async function () {
+    const time = seconds*1000 
+    await browser.pause(time)
+})
