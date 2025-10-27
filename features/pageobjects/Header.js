ae66758ac355f4bd9f30bcadfc3dc16774045806
ihar <igorsakovich1995@gmail.com>
2025-10-27 23:03:09 +0300

    fix xpath for profile

--- features/pageobjects/Header.js
+++ features/pageobjects/Header.js
@@ -3,7 +3,7 @@ class Header {
   get locatorsMap() {
         return {
           'Вход': '//*[@data-test = "main_signin"]',
-          'Профиль': '//*[@data-test="main_profile_login_name"]'
+          'Профиль': '//*[@data-test="main_profile_login_name"]//*[contains(@href ,"/profile")]'
         };
     }
 }
