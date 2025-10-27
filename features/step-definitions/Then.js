e43e320c0c9557143a38f8ec0fc3d127a3d799b5
Ihar Sakovich <i.sakovich@jetmail.cc>
2025-10-27 18:32:43 +0300

    asdf

--- features/step-definitions/Then.js
+++ features/step-definitions/Then.js
@@ -6,11 +6,3 @@ Then('Я вижу {string} на странице {string}', async function (elem
     const locator = await getLocator(element, page)
     await expect(locator).toBeDisplayed();
 }); 
-
-// Then('Я вижу {string} и {string} на странице {string}', async function(element_1, element_2){
-
-//     const locator_1 = getLocator(element_1)
-//     const locator_2 = getLocator(element_2)
-//     await expect(locator_1).toBeDisplayed();
-//     await expect(locator_2).toBeDisplayed();
-// });
